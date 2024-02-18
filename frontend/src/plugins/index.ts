67f5d292cf32131e596b27380f04b8a1e62cddfd
Eduardo <eduardocappellotto@gmail.com>
2024-02-18 13:48:51 -0300

    feat: :construction: configuration files for the frontend, router, store, plugins

--- frontend/src/plugins/index.ts
+++ frontend/src/plugins/index.ts
@@ -1,18 +1,9 @@
-/**
- * plugins/index.ts
- *
- * Automatically included in `./src/main.ts`
- */
-
-// Plugins
 import vuetify from './vuetify'
 import pinia from '../store'
 import router from '../router'
-
-// Types
 import type { App } from 'vue'
 
-export function registerPlugins (app: App) {
+export function registerPlugins(app: App) {
   app
     .use(vuetify)
     .use(router)
